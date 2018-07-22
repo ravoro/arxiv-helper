a5428cd2abf537cf3e0924c733f0d0c6b9e4965f
roman <ravoro@gmail.com>
2018-07-22 17:07:45 -0400

    Add arxiv article fetcher that saves articles to django db

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from mptt.admin import MPTTModelAdmin, TreeRelatedFieldListFilter
 
-# Register your models here.
+from .models import Article, Category
+
+admin.site.site_header = 'Arxiv Helper'
+admin.site.site_title = admin.site.site_header
+admin.site.site_url = None
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'title', 'categories_str']
+    list_filter = [('categories', TreeRelatedFieldListFilter)]
+
+
+class CategoryAdmin(MPTTModelAdmin):
+    pass
+
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Category, CategoryAdmin)
