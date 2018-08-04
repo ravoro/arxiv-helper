ed1a451aeaa397a808aa1e9e44739e91f383c1e8
roman <ravoro@gmail.com>
2018-08-04 10:22:37 -0400

    Make index page redirect to articles list

--- project/urls.py
+++ project/urls.py
@@ -1,8 +1,11 @@
 from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.urls import reverse_lazy
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    url(r'^$', RedirectView.as_view(url=reverse_lazy('admin:app_article_changelist')), name='home'),
     url(r'^', admin.site.urls),
 ]
 
