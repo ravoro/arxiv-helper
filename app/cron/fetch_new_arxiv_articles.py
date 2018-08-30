09030a0e217066f16b38a564536159d3acc2078f
roman <ravoro@gmail.com>
2018-08-29 23:22:22 -0400

    Refactor fetch script to loosen arxiv id format and to skip past pages without new articles

--- app/cron/fetch_new_arxiv_articles.py
+++ app/cron/fetch_new_arxiv_articles.py
@@ -62,7 +62,8 @@ def _append_tags(tags):
 def _get_submission_arxiv_id_or_none(submission):
     try:
         id_text = submission.select_one('dt .list-identifier a:nth-of-type(1)').text
-        id = re.match(r'^arXiv\:([\d|\.]+)$', id_text).group(1)
+        # support formats: arXiv:1605.03286 and arXiv:physics/0612096
+        id = re.match(r'^arXiv\:(.+\d+)$', id_text).group(1)
         return id
     except:
         return None
@@ -118,88 +119,88 @@ def _url_to_soup(url):
     return BeautifulSoup(html, 'html.parser')
 
 
-def _soup_to_articles(soup):
+def _soup_to_articles(soup, new_submission_date):
     """Return collection of articles (new submissions, crosslists, replacements) inside the given BeautifulSoup obj."""
     dlpage = soup.select_one('#dlpage')
     section_titles = dlpage.select('h3')
 
     if len(section_titles) != 3:
-        print('Did not have 3 section titles. section_titles: {}.'.format(section_titles))
+        logging.warning('Did not have 3 section titles. section_titles: {}.'.format(section_titles))
 
     new_submissions_title = section_titles[0]
     _assert_or_exit(new_submissions_title.text.strip().lower().startswith('new submissions for'),
                     'The first section title was not for new submissions. section_titles: {}.'.format(section_titles))
 
-    submission_date = _get_date_from_new_submissions_title_or_none(new_submissions_title)
-    _assert_or_exit(submission_date,
-                    'Could not determine the submission date.')
+    latest_submission_date = _get_date_from_new_submissions_title_or_none(new_submissions_title)
+    _assert_or_exit(latest_submission_date,
+                    'Could not determine the latest submission date.')
 
-    new_articles_count = Article.objects.filter(date_submitted__gte=submission_date).count()
-    _assert_or_exit(new_articles_count == 0,
-                    'Articles with same ({}) or newer submission date already exist in db.'.format(submission_date))
-
-    # Process new submissions
-
-    new_submissions_dl = _next_tag_sibling(new_submissions_title)
-    _assert_or_exit(new_submissions_dl.name == 'dl',
-                    'A `<dl>` did not follow the new submissions title.')
-
-    new_submissions_pairings = _pair_dt_dd_tags(new_submissions_dl.children)
-    _assert_or_exit(_validate_dt_dd_pairings(new_submissions_pairings),
-                    'A `<dl>` did not contain valid parings of only `<dt>` + `<dd>`.')
-
-    new_submissions_tags = [_append_tags(pair) for pair in new_submissions_pairings]
-    _assert_or_exit(_validate_all_submissions_have_arxiv_ids(new_submissions_tags),
-                    'A submission did not contain a valid arxiv id.')
-
-    new_submissions = [_submission_tag_to_dict(s) for s in new_submissions_tags]
-    print('Detected {} new submissions.'.format(len(new_submissions)))
-
-    # Process crosslists
+    new_submissions = []
     crosslists = []
-    if len(section_titles) > 1 and section_titles[1].text.strip().lower().startswith('cross-lists for'):
-        crosslists_title = section_titles[1]
-        _assert_or_exit(crosslists_title.text.strip().lower().startswith('cross-lists for'),
-                        'The second section title was not for crosslists. section_titles: {}.'.format(section_titles))
-
-        crosslists_dl = _next_tag_sibling(crosslists_title)
-        _assert_or_exit(crosslists_dl.name == 'dl',
-                        'A `<dl>` did not follow the crosslists title.')
-
-        crosslists_pairings = _pair_dt_dd_tags(crosslists_dl.children)
-        _assert_or_exit(_validate_dt_dd_pairings(crosslists_pairings),
-                        'A `<dl>` did not contain valid parings of only `<dt>` + `<dd>`.')
-
-        crosslists_tags = [_append_tags(pair) for pair in crosslists_pairings]
-        _assert_or_exit(_validate_all_submissions_have_arxiv_ids(crosslists_tags),
-                        'A crosslist did not contain a valid arxiv id.')
-
-        crosslists = [_submission_tag_to_dict(s) for s in crosslists_tags]
-    print('Detected {} crosslists.'.format(len(crosslists)))
-
-    # Process replacements
     replacements = []
-    _is_title_2 = len(section_titles) > 1 and section_titles[1].text.strip().lower().startswith('replacements for')
-    _is_title_3 = len(section_titles) > 2 and section_titles[2].text.strip().lower().startswith('replacements for')
-    if _is_title_2 or _is_title_3:
-        replacements_title = section_titles[2] if _is_title_3 else section_titles[1]
-        _assert_or_exit(replacements_title.text.strip().lower().startswith('replacements for'),
-                        'The third section title was not for replacements. section_titles: {}.'.format(section_titles))
 
-        replacements_dl = _next_tag_sibling(replacements_title)
-        _assert_or_exit(replacements_dl.name == 'dl',
-                        'A `<dl>` did not follow the replacements title.')
+    if latest_submission_date < new_submission_date:
+        logging.warning('Ignoring articles because no new submissions detected for ({}).'.format(new_submission_date))
+    else:
+        # Process new submissions
+        new_submissions_dl = _next_tag_sibling(new_submissions_title)
+        _assert_or_exit(new_submissions_dl.name == 'dl',
+                        'A `<dl>` did not follow the new submissions title.')
 
-        replacements_pairings = _pair_dt_dd_tags(replacements_dl.children)
-        _assert_or_exit(_validate_dt_dd_pairings(replacements_pairings),
+        new_submissions_pairings = _pair_dt_dd_tags(new_submissions_dl.children)
+        _assert_or_exit(_validate_dt_dd_pairings(new_submissions_pairings),
                         'A `<dl>` did not contain valid parings of only `<dt>` + `<dd>`.')
 
-        replacements_tags = [_append_tags(pair) for pair in replacements_pairings]
-        _assert_or_exit(_validate_all_submissions_have_arxiv_ids(replacements_tags),
-                        'A replacement did not contain a valid arxiv id.')
+        new_submissions_tags = [_append_tags(pair) for pair in new_submissions_pairings]
+        _assert_or_exit(_validate_all_submissions_have_arxiv_ids(new_submissions_tags),
+                        'A submission did not contain a valid arxiv id.')
 
-        replacements = [_submission_tag_to_dict(s) for s in replacements_tags]
-    print('Detected {} replacements.'.format(len(replacements)))
+        new_submissions = [_submission_tag_to_dict(s) for s in new_submissions_tags]
+        logging.warning('Detected {} new submissions.'.format(len(new_submissions)))
+
+        # Process crosslists
+        if len(section_titles) > 1 and section_titles[1].text.strip().lower().startswith('cross-lists for'):
+            crosslists_title = section_titles[1]
+            _assert_or_exit(crosslists_title.text.strip().lower().startswith('cross-lists for'),
+                            'The second section title was not for crosslists. section_titles: {}.'.format(section_titles))
+
+            crosslists_dl = _next_tag_sibling(crosslists_title)
+            _assert_or_exit(crosslists_dl.name == 'dl',
+                            'A `<dl>` did not follow the crosslists title.')
+
+            crosslists_pairings = _pair_dt_dd_tags(crosslists_dl.children)
+            _assert_or_exit(_validate_dt_dd_pairings(crosslists_pairings),
+                            'A `<dl>` did not contain valid parings of only `<dt>` + `<dd>`.')
+
+            crosslists_tags = [_append_tags(pair) for pair in crosslists_pairings]
+            _assert_or_exit(_validate_all_submissions_have_arxiv_ids(crosslists_tags),
+                            'A crosslist did not contain a valid arxiv id.')
+
+            crosslists = [_submission_tag_to_dict(s) for s in crosslists_tags]
+        logging.warning('Detected {} crosslists.'.format(len(crosslists)))
+
+        # Process replacements
+        _is_title_2 = len(section_titles) > 1 and section_titles[1].text.strip().lower().startswith('replacements for')
+        _is_title_3 = len(section_titles) > 2 and section_titles[2].text.strip().lower().startswith('replacements for')
+        if _is_title_2 or _is_title_3:
+            replacements_title = section_titles[2] if _is_title_3 else section_titles[1]
+            _assert_or_exit(replacements_title.text.strip().lower().startswith('replacements for'),
+                            'The third section title was not for replacements. section_titles: {}.'.format(section_titles))
+
+            replacements_dl = _next_tag_sibling(replacements_title)
+            _assert_or_exit(replacements_dl.name == 'dl',
+                            'A `<dl>` did not follow the replacements title.')
+
+            replacements_pairings = _pair_dt_dd_tags(replacements_dl.children)
+            _assert_or_exit(_validate_dt_dd_pairings(replacements_pairings),
+                            'A `<dl>` did not contain valid parings of only `<dt>` + `<dd>`.')
+
+            replacements_tags = [_append_tags(pair) for pair in replacements_pairings]
+            _assert_or_exit(_validate_all_submissions_have_arxiv_ids(replacements_tags),
+                            'A replacement did not contain a valid arxiv id.')
+
+            replacements = [_submission_tag_to_dict(s) for s in replacements_tags]
+        logging.warning('Detected {} replacements.'.format(len(replacements)))
 
     return {
         'new_submissions': new_submissions,
@@ -208,8 +209,25 @@ def _soup_to_articles(soup):
     }
 
 
+def _new_submission_date_or_exit():
+    """Check the ARXIV_CS_FEED_URL to determine whether new articles have been published across all sites."""
+
+    _cs_new_submissions_title = _url_to_soup(settings.ARXIV_CS_FEED_URL).select_one('#dlpage').select('h3')[0]
+    submission_date = _get_date_from_new_submissions_title_or_none(_cs_new_submissions_title)
+    _assert_or_exit(submission_date,
+                    'Could not determine the submission date.')
+
+    new_articles_count = Article.objects.filter(date_submitted__gte=submission_date).count()
+    _assert_or_exit(new_articles_count == 0,
+                    'Articles with same ({}) or newer submission date already exist in db.'.format(submission_date))
+
+    return submission_date
+
+
 def run():
-    print("Running 'fetch_new_articles' job at {}".format(datetime.now()))
+    logging.warning("Running 'fetch_new_articles' job at {}".format(datetime.now()))
+
+    new_submission_date = _new_submission_date_or_exit()
 
     # feeds are in priority order
     arxiv_feed_urls = [
@@ -224,53 +242,50 @@ def run():
     crosslists = []
     replacements = []
     for feed_url in arxiv_feed_urls:
-        print('Assessing url: {}'.format(feed_url))
+        logging.warning('Assessing url: {}'.format(feed_url))
         soup = _url_to_soup(feed_url)
-        articles = _soup_to_articles(soup)
+        articles = _soup_to_articles(soup, new_submission_date)
         new_submissions += articles['new_submissions']
         crosslists += articles['crosslists']
         replacements += articles['replacements']
 
-    _cs_new_submissions_title = _url_to_soup(settings.ARXIV_CS_FEED_URL).select_one('#dlpage').select('h3')[0]
-    submission_date = _get_date_from_new_submissions_title_or_none(_cs_new_submissions_title)
-
     for submission in new_submissions:
-        print('Creating "new submission": {}'.format(submission['id_arxiv']))
+        logging.warning('Creating "new submission": {}'.format(submission['id_arxiv']))
         try:
             Article.objects.create(id_arxiv=submission['id_arxiv'],
                                    html_meta=submission['html_meta'],
-                                   date_submitted=submission_date,
-                                   date_updated=submission_date,
+                                   date_submitted=new_submission_date,
+                                   date_updated=new_submission_date,
                                    is_processed=False)
         except IntegrityError:
-            print('Integrity error occurred adding "new submission" #{}. Ignoring error and proceeding.'.format(submission['id_arxiv']))
+            logging.warning('Integrity error occurred adding "new submission" #{}. Ignoring error and proceeding.'.format(submission['id_arxiv']))
 
     for crosslist in crosslists:
-        print('Creating crosslist: {}'.format(crosslist['id_arxiv']))
+        logging.warning('Creating crosslist: {}'.format(crosslist['id_arxiv']))
         try:
             Article.objects.create(id_arxiv=crosslist['id_arxiv'],
                                    html_meta=crosslist['html_meta'],
-                                   date_submitted=submission_date,
-                                   date_updated=submission_date,
+                                   date_submitted=new_submission_date,
+                                   date_updated=new_submission_date,
                                    is_processed=False)
         except IntegrityError:
-            print('Integrity error occurred adding "crosslist" #{}. Marking record as unprocessed.'.format(crosslist['id_arxiv']))
+            logging.warning('Integrity error occurred adding "crosslist" #{}. Marking record as unprocessed.'.format(crosslist['id_arxiv']))
             article = Article.objects.get(id_arxiv=crosslist['id_arxiv'])
             article.is_processed = False
             article.save()
 
     for replacement in replacements:
-        print('Creating replacement: {}'.format(replacement['id_arxiv']))
+        logging.warning('Creating replacement: {}'.format(replacement['id_arxiv']))
         try:
             # TODO: Fetch complete article details for replacement that is not yet in db
             Article.objects.create(id_arxiv=replacement['id_arxiv'],
                                    html_meta=replacement['html_meta'],
-                                   date_submitted=submission_date,
-                                   date_updated=submission_date,
+                                   date_submitted=new_submission_date,
+                                   date_updated=new_submission_date,
                                    is_processed=False)
         except IntegrityError:
-            print('Integrity error occurred adding "replacement" #{}. Marking record as unprocessed.'.format(replacement['id_arxiv']))
+            logging.warning('Integrity error occurred adding "replacement" #{}. Marking record as unprocessed.'.format(replacement['id_arxiv']))
             article = Article.objects.get(id_arxiv=replacement['id_arxiv'])
             article.is_processed = False
-            article.date_updated = submission_date
+            article.date_updated = new_submission_date
             article.save()
